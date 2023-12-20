f02ec4d28ab5e475e2c9bad4fcd56a1dc3d62606
grythman <btuguldur397@gmail.com>
2023-12-21 05:17:53 +0800

    first commit

---
+++ global-wiatprob0.c
@@ -0,0 +1,24 @@
+#include "csapp.h"
+
+/* $begin waitprob0 */
+/* $begin wasidewaitprob0 */
+int main()
+{
+    if (Fork() == 0)
+    {
+        printf("9");
+        fflush(stdout);
+    }
+    else
+    {
+        printf("0");
+        fflush(stdout);
+        waitpid(-1, NULL, 0);
+    }
+    printf("3");
+    fflush(stdout);
+    printf("6");
+    exit(0);
+}
+/* $end waitprob0 */
+/* $end wasidewaitprob0 */
